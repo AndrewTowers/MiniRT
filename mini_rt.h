cd513d8ce81465f634888b949945403b08d814bd
BlasAST <blas.siguencia1@gmail.com>
2026-07-28 14:18:12 +0200

    create main

---
+++ mini_rt.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   mini_rt.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bsiguenc <bsiguenc@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/07/28 14:02:05 by bsiguenc          #+#    #+#             */
+/*   Updated: 2026/07/28 14:09:56 by bsiguenc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINI_RT_H
+# define MINI_RT_H
+# include <stdlib.h>
+# include <unistd.h>
+# include <errno.h>
+# include <math.h>
+# include <fcntl.h>
+# include <stdio.h>
+# include <string.h>
+# include <sys/time.h>
+
+
+
+#endif
